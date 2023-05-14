9b3c97736287306e99682cef5576c88da4af19b9
Leandro Olczyk <73176252+LeandroOlczyk@users.noreply.github.com>
2023-05-14 18:04:41 -0300

    Update Sensor_de_Luz.ino

--- Sensor_de_Luz.ino
+++ Sensor_de_Luz.ino
@@ -10,33 +10,34 @@ int minimo=30;
 int maximo=50;
 
 void setup() {
-
   Serial.begin(9600);
   pinMode(ledVermelho,OUTPUT);
   pinMode(ledAmarelo,OUTPUT);
   pinMode(ledVerde,OUTPUT);
-
   pinMode(LDR,INPUT);
 }
 
 void loop(){
 
-
   valorLDR = analogRead(LDR);
   
-  if (valorLDR < minimo){ apagarLeds(); digitalWrite(ledVermelho, HIGH); }
+  if (valorLDR < minimo){ 
+    apagarLeds(); digitalWrite(ledVermelho, HIGH); 
+  }
+
+  if (valorLDR >= minimo && valorLDR < maximo){ 
+    apagarLeds(); digitalWrite(ledAmarelo, HIGH);  
+  }
   
-  if (valorLDR >= minimo && valorLDR < maximo){ apagarLeds(); digitalWrite(ledAmarelo, HIGH);  }
-  
-  if (valorLDR >=maximo ){ apagarLeds(); digitalWrite(ledVerde, HIGH); } 
+  if (valorLDR >=maximo ){ 
+    apagarLeds(); digitalWrite(ledVerde, HIGH); 
+  } 
   
   Serial.print(valorLDR);
 }
 
 void apagarLeds(){
-
     digitalWrite(ledVermelho, LOW);
     digitalWrite(ledAmarelo, LOW);
     digitalWrite(ledVerde, LOW);
-
 }
